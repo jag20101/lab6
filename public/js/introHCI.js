20c48b4737519662dd37af8b6c453882e770810a
Jorge Aguirre <jlaguirre@stanford.edu>
2014-02-12 03:20:21 -0800

    ajax lab finish round:2

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -51,7 +51,7 @@ function randomizeColors(e) {
 }
 
 function setColors(result){
-	console.log(colors);
+	var colors = result.colors.hex;
 	$('body').css('background-color', colors[0]);
 	$('.thumbnail').css('background-color', colors[1]);
 	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
